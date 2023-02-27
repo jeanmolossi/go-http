c0938a5261faecb2a1c277636d24fa91e63b636b
Jean Molossi <jean.carlosmolossi@gmail.com>
2023-02-27 01:07:14 -0300

    chore: update constant value

--- helpers_test.go
+++ helpers_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestIsAcceptable(t *testing.T) {
-	require.True(t, isAcceptable(`application\/(hal[+\-])?json`, "application/json; charset utf-8"))
-	require.True(t, isAcceptable(`application\/(hal[+\-])?json`, "application/json; charset utf-8"))
-	require.False(t, isAcceptable(`application\/(hal[+\-])?json`, "text/html; charset utf-8"))
+	require.True(t, isAcceptable(applicationJSON, "application/json; charset utf-8"))
+	require.True(t, isAcceptable(applicationJSON, "application/json; charset utf-8"))
+	require.False(t, isAcceptable(applicationJSON, "text/html; charset utf-8"))
 }
